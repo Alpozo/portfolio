38cf1e2787d67d3d81e5682d6fee34c908324928
Alejandro <alejandrodelpozo91@gmail.com>
2025-01-07 22:07:40 +0100

    feat: redirect to main page on avatar click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
 import './Header.css';
-import profilePicture from "../../assets/profile-picture.jpg";   
+import profilePicture from "../../assets/profile-picture.jpg";
 
 export const Header = () => {
   return (
-        <div className='header-wrapper'>
-          <img className="profile-picture" src={profilePicture} alt="Alejandro"></img>
-          <div>
-            <p className="text-header">
+    <div className='header-wrapper'>
+      <a href='https://alpozo.github.io/portfolio/'>
+        <img className="profile-picture" src={profilePicture} alt="Alejandro"></img>
+        <div>
+          <p className="text-header">
             Alejandro del Pozo
-            </p>
-            <p className="text-body-regular">
+          </p>
+          <p className="text-body-regular">
             Digital Product Designer
-            </p>
-          </div>
-       </div>
+          </p>
+        </div>
+      </a>
+    </div>
   )
 }
